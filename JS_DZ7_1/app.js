ce67123b2fd3d714365806acc72e6e016e2daf75
Oleg Garckusha <garckushaoleg@gmail.com>
2019-03-19 13:04:47 +0200

    Добавил второй аргумент в функцию

--- JS_DZ7_1/app.js
+++ JS_DZ7_1/app.js
@@ -1,31 +1,31 @@
 const obj = { name: 'Alex', age: 33, adress: { country: 'UA', city: 'Dnipro' } };
-let objCopy = {};//возвращаемый объект в функции
-let interim = {};//промежуточный объект в функции
-let counter = 0;//счётчик
-let keyTwo;//переменная для фиксирования свойста, у которого значение объект
+let objCopy = {};// копия объекта
+let counter = 0;// счётчик
+let valueObject;// переменная сохраняет название свойства, у которого значение объект
 
-function copy(obj) {
+function copy(objForCopy, objCopy) {
+    let interim = {};//промежуточный объект в функции
 
-    for (let keyOne in obj) {
+    for (let keyOne in objForCopy) {
 
-        if (typeof (obj[keyOne]) == "object") {
+        if (typeof (objForCopy[keyOne]) == "object") {
             counter++;
-            keyTwo = keyOne;
-            return copy(obj[keyOne]);
+            valueObject = keyOne;
+            return copy(objForCopy[keyOne], objCopy);
         }
 
         //Если счётчик ноль, то копируем первый уровень, если не ноль, то внутренний уровень
-        if (!counter) objCopy[keyOne] = obj[keyOne];
-        else interim[keyOne] = obj[keyOne];
+        if (!counter) objCopy[keyOne] = objForCopy[keyOne];
+        else interim[keyOne] = objForCopy[keyOne];
     }
-    //Если не ноль, то присваиваем зафиксированному свойству значение
-    if (counter) objCopy[keyTwo] = interim;
+    //Если не ноль, то присваиваем зафиксированному свойству значение-объект
+    if (counter) objCopy[valueObject] = interim;
 
     return objCopy;
 }
 
 
-const objTwo = copy(obj);
+const objTwo = copy(obj, objCopy);
 
 //Проверка
 objTwo.name = 'Oleg';
