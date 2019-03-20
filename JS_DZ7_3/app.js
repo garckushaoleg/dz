af758d28388ecde0652a34b49dcadd5b517bb64c
Oleg Garckusha <garckushaoleg@gmail.com>
2019-03-20 14:47:31 +0200

    Добавил проверку на массив

--- JS_DZ7_3/app.js
+++ JS_DZ7_3/app.js
@@ -2,20 +2,30 @@ const obj = { name: 'Alex',
               age: 33, 
               findwork: true, 
               preferences: null,
+              arr: [0,1,2,3,4,5],
               adress: { country: 'UA', city: 'Dnipro' } };
 
 
 function copy(objForCopy) {
     let objCopy = {};
+    let arrCopy = [];
     for (let key in objForCopy) {
-
-        if (typeof (objForCopy[key]) == "object" && objForCopy[key] !== null) {
+        //Если массив, то запускаем глубокое копирование
+        if (Array.isArray(objForCopy[key])) {
+            objCopy[key] = copy(objForCopy[key]);
+        } 
+        else if (typeof (objForCopy[key]) == "object" && objForCopy[key] !== null) {
             // Если объект, то вызываю ещё раз функцию и передаю свойству результат-объект
             objCopy[key] = copy(objForCopy[key]);
         }
+        //Если проверка на массив true, то копируем массив
+        else if (Array.isArray(objForCopy)) arrCopy[key] = objForCopy[key];
         else objCopy[key] = objForCopy[key];
-    }
 
+    }
+    //Если проверка на массив true, то возвращаем массив
+    if (Array.isArray(objForCopy)) return arrCopy;
+    
     return objCopy;
 }
 
@@ -24,5 +34,6 @@ const objTwo = copy(obj);
 //Проверка
 objTwo.name = 'Oleg';
 objTwo.adress.country = 'USA';
+objTwo.arr[1] = 'serhserthrth';
 console.log(obj);
 console.log(objTwo);
