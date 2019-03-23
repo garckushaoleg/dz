4f676717fc0c457e04695a384989868e67f76611
Oleg Garckusha <garckushaoleg@gmail.com>
2019-03-23 16:30:26 +0200

    Убрал лишний цикл, лишние промпты, изменил название функций, усовершенствовал функции, убрал лишние переменные

--- JS_DZ8/app.js
+++ JS_DZ8/app.js
@@ -1,44 +1,37 @@
-let arrNumbers = [];
-
-//Контроль ввода имени
-function toControl(userAnswer) {
-    if (userAnswer === null || Number(userAnswer) == 0) {
-        userAnswer = prompt('Введите Ваше имя пожалуйста', ''); 
-        return toControl(userAnswer);
+//Возвращает имя юзера
+function getUserName() {
+    let userAnswer = prompt('Введите Ваше имя пожалуйста', ''); 
+    if (userAnswer === null || userAnswer.trim() === '' || !isNaN(Number(userAnswer))) {
+        return getUserName();
     } else {
         return userAnswer;
     }
 }
 
 
-//Валидация
-function validate(answerUser) {
-    if (!!Number(answerUser) && answerUser <= 100) {
+//Возвращает число
+function getNumber() {
+    let answerUser = prompt('Введите число от 0 до 100', ''); 
+    if (!isNaN(Number(answerUser)) && answerUser >= 0 && answerUser <= 100 
+        && answerUser !== null && answerUser.trim() !== '') {
         return answerUser;
     } else {
-        answerUser = prompt('Введите ещё раз', ''); 
-        return validate(answerUser);
+        return getNumber();
     }
 }
 
 //Добавление в тег h1 приветствия с именем юзера
-let answer = prompt('Как тебя зовут?', '');
 let h1 = document.body.getElementsByTagName('h1') [0];
-h1.innerText = 'Привет ' + toControl(answer) + '!';
+h1.innerText = 'Привет ' + getUserName() + '!';
 
-//Ввод и проверка числа
-answer = prompt('Введите число от 0 до 100', '');
-let answerTrue = validate(answer);
-//Формирование массива чисел от 1 до answerTrue
-for(let i=0; i<answerTrue; i++) {
-    arrNumbers.push(i+1);
-}
 // Создание и добавление тега ul
 let ul = document.createElement('ul');
 document.body.appendChild(ul);
-// Теги li с числами из массива
-arrNumbers.forEach(function(item) {
-        ul.innerHTML = ul.innerHTML + '<li>' + item + '</li>';
-    })
+//Ввод и проверка числа
+let answerTrue = getNumber();
+//Теги li с числами из массива
+for(let i = 0; i <= answerTrue; i++) {
+    ul.innerHTML = ul.innerHTML + '<li>' + i + '</li>';
+}
 
 
