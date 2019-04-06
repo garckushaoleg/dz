46ed5fda211261356b6c0283bc7a1feabdd2e416
Oleg Garckusha <garckushaoleg@gmail.com>
2019-04-07 01:39:59 +0300

    Изменил формулу расчёта средних оценок студента и группы, сделал методы общими, испольховал arrow function в методах reduce и filter

--- JS_DZ12/app.js
+++ JS_DZ12/app.js
@@ -1,60 +1,65 @@
-function Student (name, rating) {
+function Student (name, arrMarks) {
     this.name = name;
-    this.allWorksStudent = rating.length;// Общее количество работ студента
+    this.arrMarks = arrMarks;
 
     // Возвращает среднюю оценку
-    this.averageMark = function() {
-        return rating.reduce(function(result, item) {
-            return result + item/2;
-        }, 0)
-    }
+    this.averageMark = averageMark;
 
     //Возвращает количество сданных работ
-    this.worksDone = function() {
-        let arr = rating.filter(function(number) {
-            return number>0
-        });
-
-        return arr.length
-    }
+    this.worksDone = worksDone;
 
     //Добавляет ещё одну оценку студенту
-    this.addMark = function(item) {
-        rating.push(item);
-        return rating
-    }
+    this.addMark = addMark;
 }
 
-//Возвращает среднюю оценку по группе
+//Будущий метод. Возвращает среднюю оценку.
 function averageMark() {
-    return students.reduce(function(result, item) {
-        return result + item.averageMark()/2;
-    }, 0)
+    let sumAllMarks = this.arrMarks.reduce( (result, item) => result + item, 0);
+    return Math.round(sumAllMarks/this.arrMarks.length);
+}
+
+//Будущий метод. Возвращает количество сданных работ.
+function worksDone() {
+    let arr = this.arrMarks.filter( (item) => item>0 );
+    return arr.length
+}
+
+//Будущий метод. Добавляет ещё одну оценку студенту.
+function addMark(item) {
+    this.arrMarks.push(item);
+    return this.arrMarks
+}
+
+
+//Возвращает среднюю оценку по группе
+function getGroupAverageMark() {
+    let sumAllMarks = students.reduce( (result, item) => result + item.averageMark(), 0);
+    return Math.round(sumAllMarks/students.length);
 }
 
 //Возвращает процент сданных работ по группе
 function completePercent() {
-    let allSubmittedWorks = students.reduce(function(result, item) {
-        return result + item.worksDone();
-    }, 0);
+    let allSubmittedWorks = students.reduce( (result, item) => result + item.worksDone(), 0);
 
-    let allWorks = students.reduce(function(result, item) {
-        return result + item.allWorksStudent;
-    }, 0);
+    let allWorks = students.reduce( (result, item) => result + item.arrMarks.length, 0);
+
     return Math.round(allSubmittedWorks/allWorks *100)
 }
 
 
 const students = [ 
     new Student('Student 1', [10,9,8,0,10]),
+    new Student('Student 12', [10,0,8,0,3,4]),
     new Student('Student 12', [10,0,8,0,3,4])
-   ];
+];
 
 console.log(students);
 console.log(students[0].averageMark());
 console.log(students[1].averageMark());
+console.log(students[2].averageMark());
 console.log(students[0].worksDone());
 console.log(students[0].addMark(5));
+console.log(students[0].averageMark());
 
-console.log(averageMark());
+console.log(getGroupAverageMark());
 console.log(completePercent());
