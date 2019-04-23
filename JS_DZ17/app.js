907d3fd675269db8d5ef67cd25ee6e71f6a9a641
Oleg Garckusha <garckushaoleg@gmail.com>
2019-04-23 17:16:15 +0300

    Добавил функцию инициализации, теперь не клонирую узел, а сразу перемещаю все body, улучшил логику show() и open(), подабовлял после ифа фигурные скобки

--- JS_DZ17/app.js
+++ JS_DZ17/app.js
@@ -3,41 +3,81 @@ class Tabset{
         this.element = element;
     }
 
-    //Показать таб
-    show(index) {
-        if (index == undefined) this.index = 0
-        else this.index = --index;
-
+    //Перемещаем все body элементы
+    moveBodyElements() {
         this.element.classList.add('tabset-container');
-
         this.elements = Array.prototype.slice.call(this.element.children);
+
+        //Закрываю все body и перемещаю все body, кроме body первого пункта, в первый пункт
+        this.elements.forEach ((item, i) => {
+            let bodyElement = item.children[1];
+            bodyElement.classList.add('tabset-body-close');
+            if (!i) {
+                return;
+            }
+            this.element.children[0].appendChild(bodyElement);
+        })
+    }
+
+    //Показать Таб
+    show(index) {
+        this.moveBodyElements();
         
-        this.elements.forEach((item, i) => {
-            item.children[1].classList.add('tabset-body-close');
+        if (index == undefined) { 
+            this.index = 0;
+        }
+        else { 
+            this.index = --index;
+        }
 
-            if (this.index == i) {
-                item.children[0].classList.add('tabset-heading-activate');
+        this.open();
+    }
 
-                let openTab = item.children[1].cloneNode(true);
-
-                openTab.classList.remove('tabset-body-close');
-                openTab.classList.add('tabset-body-open');
-
-                this.elements[0].appendChild(openTab);
-            }
-            else {
-                item.children[0].classList.add('tabset-heading-deactivate');
-            }
-        });
-
+    //Инициализация
+    init() {
+        this.show();
         this.element.addEventListener('click', this.onOpenTab.bind(this));
     }
 
-    //Обработчик клика
+    //Обработчик
     onOpenTab() {
-        this.parentElement = event.target.parentElement;
+        let parent = event.target.parentElement;
+        
+        //Узнаю индекс родительского элемента
+        this.elements.forEach((item, i ) => {
+            if (parent == item) {
+                this.index = i;
+            }
+        })
 
         this.open();
+    }
+
+    //Открыть таб
+    open() {
+        // Создаю массив из дочерних элементов первого пункта
+        this.elementsFirstItem = Array.prototype.slice.call(this.element.children[0].children);
+
+        //Закрываю все дочерние элементы первого пункта кроме нулевого, т.е. кроме heading
+        this.elementsFirstItem.forEach((item, i) => {
+            if (!i) {
+                return;
+            }
+            item.className = 'tabset-body tabset-body-close';
+        })
+
+        this.deactivateAllHeading();
+
+        //Активирую необходивый heading и открываю необходимый body
+        this.elements.forEach((item, i ) => {
+            if (this.index == i) {
+                item.children[0].classList.remove('tabset-heading-deactivate');
+                item.children[0].classList.add('tabset-heading-activate');
+
+                this.element.children[0].children[i+1].classList.remove('tabset-body-close');
+                this.element.children[0].children[i+1].classList.add('tabset-body-open');
+            }
+        })
     }
 
     //Деактивировать все heading
@@ -49,47 +89,35 @@ class Tabset{
         })
     }
 
-    //Открыть таб
-    open() {
-        this.deactivateAllHeading();
-        let headingElement = this.element.children[this.index].children[0];
-
-        if (headingElement.classList == 'tabset-heading tabset-heading-deactivate') {
-
-            headingElement.classList.remove('tabset-heading-deactivate');
-            headingElement.classList.add('tabset-heading-activate');
-
-            let bodyElement = headingElement.nextElementSibling;
-
-            let bodyElementClone = bodyElement.cloneNode(true);
-            bodyElementClone.classList.remove('tabset-body-close');
-            bodyElementClone.classList.add('tabset-body-open');
-
-            this.element.children[0].children[2].remove();
-            this.element.children[0].appendChild(bodyElementClone);
-
-        }
-    }
-
     //Следующий таб
     next() {
         this.index++;
-        if (this.index == this.element.children.length) this.index = 0;
+        
+        if (this.index == this.element.children.length) {
+            this.index = 0;
+        }
         this.open();
     }
 
     //Предыдущий таб
     prev() {
-        if (this.index == 0) this.index = this.element.children.length-1
-        else this.index--;
+        if (this.index == 0) {
+            this.index = this.element.children.length-1;
+        }
+        else {
+            this.index--;
+        }
         this.open();
     }
 }
 
 const tabs = new Tabset(document.getElementById('container'));
 
-tabs.show();
+tabs.init();
 // tabs.show(2);
 // tabs.next();
+// tabs.next();
+// tabs.prev();
+// tabs.prev();
 // tabs.prev();
 // tabs.prev();
