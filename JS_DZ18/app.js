a8838270fe60eab6bd208386ec991d7b93936f21
Oleg Garckusha <garckushaoleg@gmail.com>
2019-04-27 17:15:22 +0300

    Изменил названия некоторых переменных и функция, создаю тадлицу при помощи шаблонизации, накапливаю html в отдельной переменной, общую часть url вынес в отдельную константу, из request обратно сделал самовызывающуюся функцию

--- JS_DZ18/app.js
+++ JS_DZ18/app.js
@@ -7,28 +7,25 @@ class Users{
     }
 
     init() {
-        this.getAnswer('get', URL_USERS, (responseText) => this.sortUserData(responseText), true);
-        this.element.addEventListener('click', this.onReceiveUserPosts.bind(this));
+        request('get', URL + PATH_USERS, (responseObject) => this.displayUserData(responseObject), true);
+        this.element.addEventListener('click', this.onClickOnUserString.bind(this));
     }
 
-    //Получение ответа на запрос
-    getAnswer(method, URL, callback, async) {
-        let getAnswerToRequest = request();
-        getAnswerToRequest(method, URL, callback, async);
-    }
-
-    //Сортирую данные о юзерах по соответствующим столбцам
-    sortUserData(responseText) {
+    //Отображаю данные о юзерах
+    displayUserData(responseObject) {
+        let totalInternalHTML = '';
         let userTemplate = document.getElementById('userTemplate').innerHTML;
 
-        responseText.forEach((item) => {
+        responseObject.forEach((item) => {
 
-            this.element.children[1].innerHTML = this.element.children[1].innerHTML + userTemplate
+            totalInternalHTML = totalInternalHTML + userTemplate
                                        .replace('{{id}}', item.id)
                                        .replace('{{name}}', item.name)
                                        .replace('{{phone}}', item.phone)
                                        .replace('{{email}}', item.email);
         });
+
+        this.element.children[1].innerHTML = totalInternalHTML;
     }
 
     //Меняю бэкграунд строки
@@ -47,63 +44,56 @@ class Users{
     //Создание таблицы для постов юзера
     createTable() {
         this.table = document.createElement('table');
-        let thead = document.createElement('thead');
-        this.tbody = document.createElement('tbody');
-        let tr = document.createElement('tr');
-        let thOne = document.createElement('th');
-        let thTwo = document.createElement('th');
-        thOne.textContent = 'Title';
-        thTwo.textContent = 'Body';
+        this.table.innerHTML = document.getElementById('tableTemplate').innerHTML;
         document.body.appendChild(this.table);
-        this.table.appendChild(thead);
-        this.table.appendChild(this.tbody);
-        thead.appendChild(tr);
-        tr.appendChild(thOne);
-        tr.appendChild(thTwo);
     }
 
     //Обработчик на получение постов юзера
-    onReceiveUserPosts(e) {
+    onClickOnUserString(e) {
         this.id = e.target.parentElement.dataset.userId;
 
         this.toggleBackground(e);
 
-        this.getAnswer('get', URL_POSTS + this.id, (responseText) => this.sortDataUserPosts(responseText), true);
+        request('get', URL + PATH_POSTS + this.id, (responseObject) => this.displayUserPosts(responseObject), true);
 
         if (this.toggle) this.table.remove();
 
         this.createTable();
     }
     
-    //Сортировка постов юзера по соответствующим столбцам
-    sortDataUserPosts(responseText) {
+    //Отображаю посты юзера
+    displayUserPosts(responseObject) {
+        let totalInternalHTML = '';
+
         let postTemplate = document.getElementById('postTemplate').innerHTML;
 
-        responseText.forEach((item) => {
+        responseObject.forEach((item) => {
             this.toggle = true;
 
-            this.tbody.innerHTML = this.tbody.innerHTML + postTemplate
+            totalInternalHTML = totalInternalHTML + postTemplate
                                        .replace('{{title}}', item.title)
                                        .replace('{{body}}', item.body);
-        })
+        });
+
+        this.table.children[1].innerHTML = totalInternalHTML;
     }
 }
 
-const URL_USERS = 'https://jsonplaceholder.typicode.com/users';
-const URL_POSTS = 'https://jsonplaceholder.typicode.com/posts?userId='
+const URL = 'https://jsonplaceholder.typicode.com/';
+const PATH_USERS = 'users';
+const PATH_POSTS = 'posts?userId=';
 
 
 var request = function() {
     var xhr = new XMLHttpRequest();
     return function( method, url, callback, async) {
-        xhr.open( method, url, async);
-        xhr.send();
         xhr.onload = function() {
             callback(JSON.parse(xhr.responseText));
         };
-        
+        xhr.open( method, url, async);
+        xhr.send();
     };
-};
+}();
 
 const usersList = new Users( document.getElementById('usersListTable') );
 
