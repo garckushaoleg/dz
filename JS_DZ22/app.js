88560f316ff333adaaf6d5941318b56bc8dbf3a4
Oleg Garckusha <garckushaoleg@gmail.com>
2019-05-19 03:20:14 +0300

    Теперь все объекты хранятся в одном массиве доступные по одному ключу из localStorage, теперь стикеры сохраняют свои позиции и после перезагрузки страницы и после закрытия некоторых стикеров, закешировал что смог

--- JS_DZ22/app.js
+++ JS_DZ22/app.js
@@ -1,29 +1,23 @@
-let key = 0;
+const key = '4AD5';
+
+let IdOfMovedItem;
+
+let addButton = $('#add');
+let addForm = $('#add-form');
+let area = $('#area');
+let sticker = $('#sticker');
 
 init();
 
 function init() {
-    displayExistingItems();
+    if (getItemFromLocalStorage()) createSticker();
 
-    $('#add').click(onAddButtonClick)
-}
-
-//Показать уже имеющиеся значения из локального хранилища
-function displayExistingItems() {
-    if (localStorage.length>0) {
-        for (let i=0; i<localStorage.length; i++) {
-            key = localStorage.key(i);
-
-            createSticker();
-        }
-
-        key = localStorage.length;
-    }
+    addButton.click(onAddButtonClick)
 }
 
 //Обработчик на кнопку Добавить
 function onAddButtonClick() {
-    $('#add').after($('#add-form').html());
+    addButton.after(addForm.html());
 
     $('#save').click(onSaveButtonClick);
 
@@ -37,8 +31,6 @@ function onSaveButtonClick() {
 
     createSticker();
 
-    key++;
-    
 }
 
 //Создать стикер
@@ -48,39 +40,67 @@ function createSticker() {
     $('.close-button').click(onCloseButtonClick);
 
     $('.sticker-title').mouseenter(onAppearanceOfCursorOverTitle);
+
+    $('.sticker-title').mouseleave(onExitFromCursorArea);
+}
+
+//Получить значение с локального хранилища по ключу
+function getItemFromLocalStorage() {
+    return jQuery.parseJSON(localStorage.getItem(key));
 }
 
 //Записать значение в локальное хранилище по ключу
 function setItemToLocalStorage() {
-    localStorage.setItem(key, JSON.stringify({title: $('#title').val(), description: $('#description').val(), style: ''}));
+    let arrItem;
+    
+    if (getItemFromLocalStorage()) {
+        arrItem = getItemFromLocalStorage();
+    } else {
+        arrItem = [];
+    }
+
+    arrItem.push({title: $('#title').val(), description: $('#description').val(), style: ''});
+    localStorage.setItem(key, JSON.stringify(arrItem));
 }
 
 //Отобразить значение с локального хранилища по ключу
 function displayItemFromLocalStorage() {
-    let value = jQuery.parseJSON(localStorage.getItem(key));
+    let value = getItemFromLocalStorage();
 
-    $('#area').append($('#sticker').html().replace('{{id}}', key)
-                                          .replace('{{sticker-title}}', value.title)
-                                          .replace('{{sticker-description}}', value.description));
+    area.html('');
+    value.forEach((item, index) => {
+        if (item === null) return;
+        area.append(sticker.html().replace('{{id}}', index)
+                                          .replace('{{sticker-title}}', item.title)
+                                          .replace('{{sticker-description}}', item.description));
 
-    $('#' + key).attr('style', value.style);
+        $( '#' + index ).attr('style', item.style);
+    });
 }
 
 //Закрыть стикер и удалить значение с локального хранилища по ключу
 function onCloseButtonClick(event) {
-    let id = $(event.target).closest('div').attr('id')
-    localStorage.removeItem(id);
+    let id = $(event.target).closest('div').attr('id');
+    let arrItem = getItemFromLocalStorage();
+    delete arrItem[id];
+    localStorage.setItem(key, JSON.stringify(arrItem));
     $(event.target).closest('div').remove();
 }
 
 //Обработчик на появление курсора над заголовком
-function  onAppearanceOfCursorOverTitle(event) {
+function onAppearanceOfCursorOverTitle(event) {
     let id = $(event.target).closest('div').attr('id');
+    IdOfMovedItem = id;
     $( "#" + id ).draggable({ cancel: ".sticker-description" });
+}
 
-    let style = ($( "#" + id ).attr('style'));
-    let value = jQuery.parseJSON(localStorage.getItem(id));
-    localStorage.setItem(id, JSON.stringify({title: value.title, description: value.description, style: style}));
+//Обработчик на выход курсора из области элемента
+function onExitFromCursorArea() {
+    let style = ($( "#" + IdOfMovedItem ).attr('style'));
+    let value = getItemFromLocalStorage();
+    value[IdOfMovedItem].style = style;
+
+    localStorage.setItem(key, JSON.stringify(value));
 }
 
 //Закрыть форму добавления стикера
